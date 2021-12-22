f31bfa536f960316e4613f43ba234bff7ec02f2a
Andrew <apollock92@hotmail.com>
2021-12-22 18:48:52 +0000

    Adding optimal K plot

--- 3 - Clustering.R
+++ 3 - Clustering.R
@@ -4,6 +4,7 @@ library(caret)   ## Provides a nice modelling framework
 library(cluster) ## Library for clustering
 library(factoextra) ## For visualising clusters
 library(ggplot2)
+library(gridExtra)
 
 # Load in the processed data
 wine_data <- readr::read_csv("data/processed/processed_wine_data.csv")
@@ -36,7 +37,7 @@ for (k in 1:20){
 df <- df[-1,]
 
 # Plot the sum of squares
-ggplot(df, aes(x=k, y=sum_of_squares)) +
+p1 <- ggplot(df, aes(x=k, y=sum_of_squares)) +
   geom_line() +
   geom_point() +
   labs(x = "Number of Clusters", y = "Total Within Cluster Sum of Squares") +
@@ -47,13 +48,15 @@ ggplot(df, aes(x=k, y=sum_of_squares)) +
 df$diff <- c(0, round(diff(df$sum_of_squares, lag = 1, differences = 1),0))
 
 # Plot these decreases in TSS
-ggplot(df[2:20,], aes(x=k, y=abs(diff))) +
+p2 <- ggplot(df[2:20,], aes(x=k, y=abs(diff))) +
   geom_line() +
   geom_point() +
   labs(x = "Number of Clusters", y = "Reduction in Within Cluster TSS") +
   theme_bw()
 # From 4 clusters onwards this decrease plateaus 
 
+# Save the num clusters plots
+ggsave("plots/Plot4_Optimal_K.png", marrangeGrob(grobs = list(p1, p2), ncol=2, nrow = 1, top = element_blank()))
 
 ## Based on this analysis I'll use K = 4
 k4 <- kmeans(centered_data, centers = 4, nstart = 15, iter.max = 20)
@@ -93,12 +96,17 @@ featurePlot(x = wine_data[, 1:11],
 )
 
 
-## Format the data for my dashboard
+## How many principal components are needed?
+summary(prcomp(wine_data[,1:11], scale. = TRUE, center = TRUE))
+# The first 3 PC explain 52% of the variance
+
+## Calculate the first 3 PC for the dashboard
 pca_model <- preProcess(wine_data[,1:11], method = "pca", pcaComp = 3)
 pca_data <- predict(pca_model, wine_data[,1:11])
 
+## Append the PCs to the original data
 final_wine_data <- cbind(wine_data, pca_data) %>% select(PC1, PC2, PC3, everything())
 
-# Save this dataset for use in my dashboard
+# Save this dataset for use in the dashboard
 readr::write_csv(final_wine_data, "data/processed/final_wine_data.csv")
 
